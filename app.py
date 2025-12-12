92630a4954a6906ef3834c46ec0ae10f7a3245e5
ruthvika-29-11 <245122748011@mvsrec.edu.in>
2025-12-12 11:30:49 +0530

    cookies

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def create_app():
 
     app.config.from_object(Config)
 
-    CORS(app, resources={r"/api/*": {"origins": "*"}})
+    CORS(app,supports_credentials=True, resources={r"/api/*": {"origins": "*"}})
 
     # This must be called before accessing the database engine or session with the app.
     db.init_app(app)
